112bd95c9ef5dac647ae028e3db05613d65f068c
Daniela Restrepo <danireal1@hotmail.com>
2019-09-23 21:24:54 -0500

    primer commit

---
+++ WindowsFormsFacturador/Home.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsFacturador
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHome));
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.btnFacturador = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.PictureBoxHome = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxHome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Location = new System.Drawing.Point(26, 25);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(63, 13);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido!";
+            // 
+            // btnFacturador
+            // 
+            this.btnFacturador.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFacturador.Location = new System.Drawing.Point(38, 62);
+            this.btnFacturador.Name = "btnFacturador";
+            this.btnFacturador.Size = new System.Drawing.Size(75, 23);
+            this.btnFacturador.TabIndex = 1;
+            this.btnFacturador.Text = "Facturador";
+            this.btnFacturador.UseVisualStyleBackColor = true;
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnProductos.Location = new System.Drawing.Point(189, 62);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(75, 23);
+            this.btnProductos.TabIndex = 2;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnClientes.Location = new System.Drawing.Point(337, 62);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(75, 23);
+            this.btnClientes.TabIndex = 3;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // PictureBoxHome
+            // 
+            this.PictureBoxHome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PictureBoxHome.Image = ((System.Drawing.Image)(resources.GetObject("PictureBoxHome.Image")));
+            this.PictureBoxHome.Location = new System.Drawing.Point(38, 114);
+            this.PictureBoxHome.Name = "PictureBoxHome";
+            this.PictureBoxHome.Size = new System.Drawing.Size(414, 172);
+            this.PictureBoxHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxHome.TabIndex = 4;
+            this.PictureBoxHome.TabStop = false;
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 312);
+            this.Controls.Add(this.PictureBoxHome);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.btnFacturador);
+            this.Controls.Add(this.lblBienvenido);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormHome";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxHome)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Button btnFacturador;
+        private System.Windows.Forms.Button btnProductos;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.PictureBox PictureBoxHome;
+    }
+}
+
